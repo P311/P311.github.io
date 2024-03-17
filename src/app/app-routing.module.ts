e7afbb1c196b651d065f935e1e5f2b113fecdd75
linhao liu <linhao.liu98@gmail.com>
2024-03-16 20:31:14 -0400

    add main pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IntroComponent } from './intro/intro.component';
+import { ProcessComponent } from './process/process.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'intro', component: IntroComponent },
+  { path: 'process', component: ProcessComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'showcase', component: ShowcaseComponent },
+  { path: '**', redirectTo: 'intro' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { IntroComponent } from './intro/intro.component';
+import { ProcessComponent } from './process/process.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    ProcessComponent,
+    ShowcaseComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
