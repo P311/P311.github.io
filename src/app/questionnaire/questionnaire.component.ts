46ec1640f72bbde2126fa751a43ca1bb12ace612
yupeng huang <frankhuangyupeng@gmail.com>
2024-03-25 21:57:43 -0400

    Removed the page for questionnaire and make it a link instead

--- src/app/questionnaire/questionnaire.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-questionnaire',
-  templateUrl: './questionnaire.component.html',
-  styleUrl: './questionnaire.component.scss',
-})
-export class QuestionnaireComponent {}
